9c681f95e2f2e89273435af56b03cc2f72f58ef0
Archana Burra <burra.archana@gmail.com>
2017-11-15 20:11:23 -0500

    readded employee macro methods

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,13 +1,13 @@
 class Employee < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  # devise :database_authenticatable, :registerable,
-  #        :recoverable, :rememberable, :trackable, :validatable
-  # validates :first_name, :last_name, :role, :date_hired, presence: true
-  # enum role: [:associate, :supervisor]
-  # scope :associates, -> { where(role: 'associate') }
-  # has_many :shifts
-  # has_many :schedules, through: :shifts
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  validates :first_name, :last_name, :role, :date_hired, presence: true
+  enum role: [:associate, :supervisor]
+  scope :associates, -> { where(role: 'associate') }
+  has_many :shifts
+  has_many :schedules, through: :shifts
 
 
 end
