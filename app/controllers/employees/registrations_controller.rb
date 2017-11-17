239191bd8be0be4711bbcb174cafdf7162390015
Archana Burra <burra.archana@gmail.com>
2017-11-16 19:49:32 -0500

    modified devise signup form, fixed a couple bugs

--- app/controllers/employees/registrations_controller.rb
+++ app/controllers/employees/registrations_controller.rb
@@ -57,4 +57,13 @@ class Employees::RegistrationsController < Devise::RegistrationsController
   # def after_inactive_sign_up_path_for(resource)
   #   super(resource)
   # end
+  private
+
+  def sign_up_params
+    params.require(:employee).permit(:first_name, :last_name, :email, :password, :password_confirmation, :date_hired)
+  end
+
+  def account_update_params
+    params.require(:employee).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :date_hired)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: "welcome#home"
-  devise_for :employees, :controllers => { :omniauth_callbacks => "employees/omniauth_callbacks" }
+  devise_for :employees, :controllers => { :omniauth_callbacks => "employees/omniauth_callbacks", :registrations => 'employees/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :admin do
