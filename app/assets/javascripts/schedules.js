a214701a6f11dee98cb75ad6f96e032701bd2689
Archana Burra <archana@betterment.com>
2018-01-04 19:48:04 -0500

    fixed bug that stopped shifts from being deleted

--- app/assets/javascripts/schedules.js
+++ app/assets/javascripts/schedules.js
@@ -26,26 +26,28 @@ function Employee(attributes){
 }
 
 Schedule.prototype.displaySchedule = function(){
+  var id = this.id;
+
  $("h1#title").append("Week of " + this.start_date.format('MMM D')
  + " - " + this.end_date.format('MMM D, YYYY'));
 
- var publishString = `<a rel="nofollow" data-method="post" href="/schedules/${this.id}/publish">Publish</a>`
+ var publishString = `<a rel="nofollow" data-method="post" href="/schedules/${id}/publish">Publish</a>`
 
- var deleteString = `<a rel="nofollow" data-method="delete" href="/schedules/${this.id}">Delete</a>`
+ var deleteString = `<a rel="nofollow" data-method="delete" href="/schedules/${id}">Delete</a>`
 
- var addShiftString =  `<a href="/schedules/${this.id}/shifts/new">Add Shift</a>`
+ var addShiftString =  `<a href="/schedules/${id}/shifts/new">Add Shift</a>`
  $("h3#header-options").append(publishString + " || " + deleteString + " || " + addShiftString);
 
  this.shifts.forEach(function(shift){
    var shiftString = shift.start_time.format('h:mma') + " - " + shift.end_time.format('h:mma') + " " + shift.employee.first_name + " " + shift.employee.last_name;
 
    if(shift.published){
-     shiftString += " - PUBLISHED - "
+     shiftString += " - PUBLISHED"
    };
 
-   shiftString += `<a href="/schedules/${this.id}/shifts/${shift.id}/edit">Edit</a>` + " | ";
+   shiftString += " - " + `<a href="/schedules/${id}/shifts/${shift.id}/edit">EDIT</a>` + " | ";
 
-   shiftString += `<a rel="nofollow" data-method="delete" href="/schedules/${this.id}/shifts/${shift.id}">Delete</a>` + "<br />";
+   shiftString += `<a rel="nofollow" data-method="delete" href="/schedules/${id}/shifts/${shift.id}">DELETE</a>` + "<br />";
 
   $("div#" + shift.date.format('dddd')).append(shiftString);
  });
