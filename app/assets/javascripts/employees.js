13d7a9ab1d79281271263f770536da009b885125
Archana Burra <archana@betterment.com>
2018-01-04 21:15:35 -0500

    added index page for employees in js

--- app/assets/javascripts/employees.js
+++ app/assets/javascripts/employees.js
@@ -9,11 +9,19 @@ function Employee(attributes){
 }
 
 Employee.prototype.displayEmployee = function(){
-  var employeeString = `<strong> Name: ${this.first_name} ${this.last_name} </strong> <br>`;
-  employeeString += `Email: ${this.email}<br>`;
-  employeeString += `Date Hired: ${this.hire_date.format('MMMM Do YYYY')}<br>`;
-  employeeString += `Role: ${this.role}<br><br><br>`;
-
+  var employeeString = `<strong> Name: ${this.first_name} ${this.last_name} </strong> <br />`;
+  employeeString += `Email: ${this.email}<br />`;
+  employeeString += `Date Hired: ${this.hire_date.format('MMMM Do YYYY')}<br />`;
+  if(this.preferences.length > 0){
+    employeeString += `Role: ${this.preferences}<br />`;
+  }
+  employeeString += `Role: ${this.role}<br /><br />`;
+  employeeString += `<a href="/supervisor/employees/${this.id}">View</a> | `;
+  employeeString += `<a href="/supervisor/employees/${this.id}/shifts">View Existing Shifts</a> | `
+  employeeString += `<a href="/supervisor/employees/${this.id}/edit">Edit</a> | `
+  employeeString += `<a rel="nofollow" data-method="delete" href="/supervisor/employees/${this.id}">Delete</a> | `
+  employeeString += `<a href="/supervisor/employees/${this.id}/shifts/new">Add a Shift</a>`
+  employeeString += '<br /><br />'
   $('div.all').append(employeeString);
 }
 
